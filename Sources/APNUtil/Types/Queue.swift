3e07387e700af0f391841dafd5c205b5a8707ceb
Aaron Nance <apnance@hotmail.com>
2023-01-19 11:10:53 -0700

    Feature: add enqueuUnique(items:)

--- Sources/APNUtil/Types/Queue.swift
+++ Sources/APNUtil/Types/Queue.swift
@@ -56,9 +56,16 @@ public class Queue<Item: Equatable> {
     }
     
     /// Enqueues`item` if it is not currently in the `Queue`
-    public func enqueueNew(item: Item) {
+    public func enqueueUnique(item: Item) {
         
         if !contains(item) { enqueue(item: item) }
+        
+    }
+
+    /// Steps through `items` enqueueing any that are not currently in the `Queue`.
+    public func enqueueUnique(items: [Item]) {
+        
+        for item in items { enqueueNew(item: item) }
         
     }
     
