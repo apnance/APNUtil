65cb202b7bbbd5f9bbd37c8328733dab2929d808
Aaron Nance <apnance@hotmail.com>
2023-08-11 14:55:43 -0700

    Feature: add method for injecting a view into a container view and stretching/constraining it ti fill the container: func constrainIn(_:)

--- Sources/APNUtil/Extensions/UIView.swift
+++ Sources/APNUtil/Extensions/UIView.swift
@@ -411,6 +411,25 @@ public extension UIView {
         
     }
     
+    func constrainIn(_ container: UIView!) -> Void {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        
+        container.addSubview(self);
+        
+        topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+        
+//        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+//        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+//        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+//        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+        
+    }
+    
 }
 
 // MARK: - Layer Manipulation
