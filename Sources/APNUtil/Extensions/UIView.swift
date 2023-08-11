cef4b63b6be61b5902a8867095592f2790f1f42d
Aaron Nance <apnance@hotmail.com>
2023-08-11 14:56:05 -0700

    Tidy: removed commented

--- Sources/APNUtil/Extensions/UIView.swift
+++ Sources/APNUtil/Extensions/UIView.swift
@@ -423,11 +423,6 @@ public extension UIView {
         bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
         leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
         
-//        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-//        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-//        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-//        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-        
     }
     
 }
