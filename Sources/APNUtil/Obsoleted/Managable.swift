8b9d25e96ca864f0fd30c398cede93e6ebc725f1
Aaron Nance <apnance@hotmail.com>
2024-07-13 19:31:38 -0700

    Add

---
+++ Sources/APNUtil/Obsoleted/Managable.swift
@@ -0,0 +1,11 @@
+//
+//  Obsoleted.swift
+//
+//
+//  Created by Aaron Nance on 7/13/24.
+//
+
+import Foundation
+
+@available(swift, obsoleted: 1.0, renamed: "Manageable")
+public protocol Managable { /*renamed -> Manageable*/ }
