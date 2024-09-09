f391f37be225df232a2720f85df19b8eea0de60f
Aaron Nance <apnance@hotmail.com>
2024-09-08 17:13:01 -0700

    Feature: extend AttributedString/NSAttributedString/String with various useful utilities

--- Sources/APNUtil/Extensions/AttributedString.swift
+++ Sources/APNUtil/Extensions/AttributedString.swift
@@ -1,8 +1,72 @@
 //
-//  File.swift
-//  
+//  AttributedString.swift
+//
 //
 //  Created by Aaron Nance on 9/8/24.
 //
 
 import Foundation
+
+public extension NSAttributedString {
+    
+    // Concatenates two `NSAttributedString`s
+    static func + (left: NSAttributedString,
+                   right: NSAttributedString) -> NSAttributedString
+    {
+        
+        let concat = NSMutableAttributedString()
+        concat.append(left)
+        concat.append(right)
+        
+        return concat
+        
+    }
+    
+}
+
+@available(iOS 15, *)
+public extension AttributedString {
+    
+    /// Returns NSAttributedString version of `self`
+    var asNSAS: NSAttributedString { NSAttributedString(self) }
+    
+    var string: String { NSAttributedString(self).string }
+    
+    var isEmpty: Bool { characters.count == 0 }
+    
+    /// Trims any leading newline characters from characters array.
+    mutating func trimLeadingNewlines() {
+        
+        trimLeading("\n")
+    }
+    
+    /// Trims any trailing newline characters from characters array.
+    mutating func trimTrailingNewlines() {
+        
+        trimTrailing("\n")
+        
+    }
+    
+    /// Trims all leading `toTrim` `Character`s
+    mutating func trimLeading(_ toTrim: Character) {
+        
+        while characters.first == toTrim { characters.removeFirst() }
+        
+    }
+    
+    /// Trims all trailing `toTrim` `Character`s
+    mutating func trimTrailing(_ toTrim: Character) {
+        
+        while characters.last == toTrim { characters.removeLast() }
+        
+    }
+    
+}
+
+@available(iOS 15, *)
+public extension String {
+    
+    /// Return a copy `self` as an `AttributedString`
+    var asAS: AttributedString { AttributedString(self) }
+    
+}
--- Tests/APNUtilTests/AttributedString.swift
+++ Tests/APNUtilTests/AttributedString.swift
@@ -1,8 +1,126 @@
 //
-//  File.swift
-//  
+//  AttributedString.swift
+//  APNUtilTests
 //
-//  Created by Aaron Nance on 9/8/24.
+//  Created by Aaron Nance on 9/24/19.
+//  Copyright © 2019 Aaron Nance. All rights reserved.
 //
 
+import XCTest
 import Foundation
+import APNUtil
+
+@available(iOS 15, *)
+class AttributedStringTests: XCTestCase {
+    
+    
+    func testNSAttributedStringAddition() {
+        
+        let final   = NSAttributedString(string: "RedGreenBlue")
+        let rg      = NSAttributedString(string: "Red") + NSAttributedString(string: "Green")
+        let rgb     = rg + NSAttributedString(string: "Blue")
+        
+        XCTAssert(rgb == final)
+        
+    }
+    
+    func test_asNSAS() {
+        
+        XCTAssert(AttributedString("Original").asNSAS == NSAttributedString("Original"))
+        
+    }
+    
+    func testString() {
+        
+        let original = "Original"
+        
+        XCTAssert(AttributedString(original).string == original)
+        XCTAssert(AttributedString("Original").string == original)
+        XCTAssertFalse(AttributedString("Fake").string == original)
+        
+    }
+    
+    
+    func testIsEmpty() {
+        
+        let empty = ""
+        
+        XCTAssert(AttributedString(empty).isEmpty)
+        XCTAssert(AttributedString("").isEmpty)
+        
+    }
+    
+    func testTrimLeadingNewLines() {
+        
+        var original = AttributedString("\n\nZipZapZoppp")
+        var trimmed  = AttributedString("ZipZapZoppp")
+        XCTAssertFalse(original == trimmed)
+        
+        original.trimLeadingNewlines()
+        XCTAssert(original == AttributedString("ZipZapZoppp"))
+        XCTAssert(original == trimmed)
+        
+    }
+    
+    func testTrimLeading() {
+        
+        var original = AttributedString("\n\nZipZapZoppp")
+        var trimmed  = AttributedString("ZipZapZoppp")
+        XCTAssertFalse(original == trimmed)
+        
+        original.trimLeading("\\")
+        XCTAssertFalse(original == trimmed)
+        XCTAssert(original == AttributedString("\n\nZipZapZoppp"))
+        
+        original.trimLeading("\n")
+        XCTAssert(original == trimmed)
+        
+        original    = AttributedString("zzXXXabc")
+        trimmed     = AttributedString("abc")
+        XCTAssertFalse(original == trimmed)
+        
+        original.trimLeading("z")
+        XCTAssertFalse(original == trimmed)
+        XCTAssert(original == AttributedString("XXXabc"))
+        
+        original.trimLeading("X")
+        XCTAssert(original == trimmed)
+        
+    }    
+    
+    func testTrimTrailing() {
+        
+        var original = AttributedString("ZipZapZoppp\n\n")
+        var trimmed  = AttributedString("ZipZapZoppp")
+        XCTAssertFalse(original == trimmed)
+        
+        original.trimTrailing("\\")
+        XCTAssertFalse(original == trimmed)
+        XCTAssert(original == AttributedString("ZipZapZoppp\n\n"))
+        
+        original.trimTrailing("\n")
+        XCTAssert(original == trimmed)
+        
+        original    = AttributedString("abcXXXzz")
+        trimmed     = AttributedString("abc")
+        XCTAssertFalse(original == trimmed)
+        
+        original.trimTrailing("z")
+        XCTAssertFalse(original == trimmed)
+        XCTAssert(original == AttributedString("abcXXX"))
+        
+        original.trimTrailing("X")
+        XCTAssert(original == trimmed)
+        
+    }
+    
+    func testString_asAS() {
+        
+        let original    = "Original"
+        let final       = AttributedString(original)
+        
+        XCTAssert(original.asAS == final)
+        
+    }
+}
+
