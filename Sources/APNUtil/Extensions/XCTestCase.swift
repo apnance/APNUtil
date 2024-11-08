d1ba16cf685489b38d45cb4a033d3a104dd279ac
Aaron Nance <apnance@hotmail.com>
2024-11-08 14:03:11 -0700

    Feature: add generic test that compares XCTAsserts two strings are equal else prints out a clear description of the different results.

---
+++ Sources/APNUtil/Extensions/XCTestCase.swift
@@ -0,0 +1,46 @@
+//
+//  XCTestCase.swift
+//  APNUtil
+//
+//  Created by Aaron Nance on 11/8/24.
+//
+
+import XCTest
+import UIKit
+
+public extension XCTestCase {
+    
+    /// `XCTAsserts` that `expected == actual` with conveniently formatted
+    /// assertion failure notice.
+    /// - Parameters:
+    ///   - expected: `expected` value
+    ///   - actual: `actual` value
+    ///   - shouldTrim: 'Bool' toggling auto-trimming leading and trailing
+    ///   whitespace from `actual` and `expected` values.
+    func check(_ expected: String, vs actual: String, shouldTrim: Bool = true) {
+        
+        var (expected, actual) = (expected,actual)
+        
+        if shouldTrim {
+            
+            expected    = expected.trim()
+            actual      = actual.trim()
+            
+        }
+        
+        XCTAssert(expected == actual,
+                    """
+                    -------
+                    Expected:
+                    \(expected)
+                    - - - -
+                    
+                    Actual:
+                    \(actual)
+                    - - - -
+                    -------
+                    """)
+        
+    }
+    
+}
