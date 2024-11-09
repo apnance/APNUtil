4bdc04d031f72198dff4a2eb92350ca4e9d975da
Aaron Nance <apnance@hotmail.com>
2024-11-09 00:21:17 -0700

    Feature: add UIColor initializer that initializes a UIColor from a hex string.

--- Sources/APNUtil/Extensions/UIColor.swift
+++ Sources/APNUtil/Extensions/UIColor.swift
@@ -134,6 +134,23 @@ public extension UIColor {
         
     }
     
+    convenience init?(hex: String) {
+        
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil /*EXIT*/ }
+        
+        let r   = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let g   = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let b   = CGFloat(rgb & 0x0000FF) / 255.0
+        
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+        
+    }
     
     // MARK: Custom User Methods
     // Find the primary/secondary color this color is closest to and score the
--- Tests/APNUtilTests/UIColor.swift
+++ Tests/APNUtilTests/UIColor.swift
@@ -34,4 +34,30 @@ final class UIColorTests: XCTestCase {
         
     }
     
+    func testColorFromHex() {
+        
+        let red = UIColor(hex: "#FF0000")!
+        XCTAssert(red == UIColor.red )
+        
+        let green = UIColor(hex: "#00FF00")!
+        XCTAssert(green == UIColor.green )
+        
+        let blue = UIColor(hex: "#0000FF")!
+        XCTAssert(blue == UIColor.blue )
+        
+        XCTAssert(UIColor(hex: UIColor.blue.hexValue!)! == UIColor.blue)
+        
+        // Don't Work
+        // NOTE: white and black don't work b/c UIColor.black and UIColor.white have different color spaces.
+        XCTAssertFalse(UIColor(hex: "#FFFFFF")! == UIColor.white)
+        XCTAssertFalse(UIColor(hex: UIColor.white.hexValue!)! == UIColor.white)
+        XCTAssertFalse(UIColor(hex: "#000000")! == UIColor.black)
+        XCTAssertFalse(UIColor(hex: UIColor.black.hexValue!)! == UIColor.black)
+        
+        // Work
+        XCTAssert(UIColor(hex: UIColor.white.hexValue!)!.hexValue == "#FFFFFF")
+        XCTAssert(UIColor(hex: UIColor.black.hexValue!)!.hexValue == "#000000")
+        
+    }
+    
 }
